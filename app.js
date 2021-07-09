563916090061d2e1f83f46006a448dbf2c8c9860
norvillie <nozkie@gmail.com>
2021-07-09 08:02:43 +0100

    test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const NodeMediaServer = require('node-media-server');
 
 const config = {
   rtmp: {
-    port: process.env.PORT || 1935 ,
+    port: 1935, //process.env.PORT || 1935 ,
     chunk_size: 60000,
     gop_cache: true,
     ping: 30,
